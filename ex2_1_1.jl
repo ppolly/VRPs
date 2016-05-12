150811406344236fd784c5b5d35afdfd8640d035
ppolly <sxc447@gmail.com>
2016-05-12 01:44:42 -0400

    commit test
    test

--- ex2_1_1.jl
+++ ex2_1_1.jl
@@ -4,10 +4,10 @@ m = Model(solver=CbcSolver())
 #m = Model(solver=GurobiSolver()) if GurobiSolver is to be used.
 
 ## matrix of travel times between node i and j. node 1 = depot
-t =[ 0 2 10 2;
-     2 0  8 4;
-    10 8  0 8;
-     2 4  8 0]
+t =[ 0 2 10 2 ;
+     2 0  8 4 ;
+    10 8  0 8 ;
+     2 4  8 0 ]
 
 T = 100 #upper limit of the vehicle travel time
 C = 10 #vehicle capacity
